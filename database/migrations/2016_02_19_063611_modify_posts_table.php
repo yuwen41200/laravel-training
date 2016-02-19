c4a2f1441b9ef2d5d9ceac88bfed98fbd70ba1c9
yuwen41200 <yuwen41200@gmail.com>
2016-02-19 14:56:21 +0800

    Unit 4, Prcatice 7.

---
+++ database/migrations/2016_02_19_063611_modify_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('content')->nullable(false)->change();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('content')->nullable()->change();
+            $table->dropForeign('posts_user_id_foreign');
+        });
+    }
+}
