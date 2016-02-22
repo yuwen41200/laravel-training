3897357702c9d3a1ca3ee35bdad520ecd960be53
yuwen41200 <yuwen41200@gmail.com>
2016-02-22 11:13:59 +0800

    Unit 7, Practice 3.

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -4,13 +4,18 @@ use Illuminate\Database\Seeder;
 
 class PostTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		foreach (range(1, 20) as $number) {
+			\App\Post::create([
+				'title' => 'title '.$number,
+				'content' => 'content '.$number
+			]);
+		}
+	}
 }
